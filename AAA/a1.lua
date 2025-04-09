cfa6226ac1aaf68d8e69ddd4e693457561383b91
dovgjm <72105520+dovgjm@users.noreply.github.com>
2025-04-09 22:17:48 +0100

    a

--- AAA/a1.lua
+++ AAA/a1.lua
@@ -2,8 +2,8 @@
 -- @provides
 --   [main] a2.lua
 --   [main] a3.lua
---   BBB/b1.txt > BBB/b1.txt
---   BBB/b2.txt > BBB/b2.txt
+--   [script] BBB/b1.txt
+--   [script] BBB/b2.txt
 
 
 reaper.ShowMessageBox("Hello World", "Script code here", 0)
