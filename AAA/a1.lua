06c4eb7c58a323fa38d14a95ea9e4e7829ed9d4d
dovgjm <72105520+dovgjm@users.noreply.github.com>
2025-04-09 22:07:45 +0100

    Update a1.lua

--- AAA/a1.lua
+++ AAA/a1.lua
@@ -3,8 +3,8 @@
 --   [nomain] .
 --   [main] a2.lua
 --   [main] a3.lua
---   [nomain] [script] BBB/b1.txt
---   [nomain] [script] BBB/b2.txt
+--   BBB/b1.txt
+--   BBB/b2.txt
 
 reaper.ShowMessageBox("Hello World", "Script code here", 0)
 
