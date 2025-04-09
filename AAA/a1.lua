b8f13d30b19a3883122c682a749f65766c399c61
dovgjm <72105520+dovgjm@users.noreply.github.com>
2025-04-10 00:29:27 +0100

    Update a1.lua

--- AAA/a1.lua
+++ AAA/a1.lua
@@ -1,12 +1,12 @@
 --
 -- @version 0.6
 --
--- @description AAA
+-- 
 -- @provides
 --   [main] AAA/a2.lua
 --   [main] AAA/a3.lua
---   AAA/BBB/b1.txt [data]
---   AAA/BBB/b2.txt [data]
+--   [data nomain] AAA/BBB/b1.txt 
+--   [data nomain] AAA/BBB/b2.txt
 --
 -- 
 
