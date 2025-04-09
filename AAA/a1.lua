d4605c1cab10b149389ce796b7ac8fe5aa1c6887
dovgjm <72105520+dovgjm@users.noreply.github.com>
2025-04-09 12:41:13 +0100

    Update a1.lua

--- AAA/a1.lua
+++ AAA/a1.lua
@@ -1,9 +1,9 @@
-@version 0.5
-@description AAA
-@metapackage
-@provides
-    [main] a2.lua
-    [main] a3.lua
-    AAA/BBB/*.txt
+-- @version 0.5
+-- @provides
+--   a1.lua
+--   AAA/BBB/*.txt
+--   [main] a2.lua
+--   [main] a3.lua
 
+reaper.ShowMessageBox("Hello World", "Script code here", 0)
 
