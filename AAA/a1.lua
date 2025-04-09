f77917eef275069a9869944bfe24a94a55bc9213
dovgjm <72105520+dovgjm@users.noreply.github.com>
2025-04-10 00:34:44 +0100

    Update a1.lua

--- AAA/a1.lua
+++ AAA/a1.lua
@@ -3,10 +3,11 @@
 --
 -- 
 -- @provides
+--   [nomain] .
 --   [main] /AAA/a2.lua
 --   [main] /AAA/a3.lua
---   [data nomain] /AAA/BBB/b1.txt 
---   [data nomain] /AAA/BBB/b2.txt
+--   [script nomain] /AAA/BBB/b1.txt 
+--   [script nomain] /AAA/BBB/b2.txt
 --
 -- 
 
